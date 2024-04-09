c097c868f61d5fdc4cc180f55fb9e3cce1469fe0
algorijun <123007169+HGive@users.noreply.github.com>
2024-04-09 17:30:11 +0900

    Time: 60 ms (76.68%), Space: 50 MB (66.60%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -3,19 +3,14 @@
  * @return {Function}
  */
 var compose = function(functions) {
-    if(functions.length===0){
-        return function(x){
-            return x;
+    functions.reverse();
+    return function(x) {
+        for(const fn of functions){
+            x = fn(x);
         }
+        return x;
     }
-   // return functions.reduceRight((prevFn, nextFn) => x => nextFn(prevFn(x)));
-    functions = functions.reverse();
-    return functions.reduce(function(prevFn,nextFn){
-        return function(x){
-            return nextFn(prevFn(x));
-        }
-    })
-}
+};
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])

4325fdefb8d1d55c9780d449bbf5fb3f8f684c69
algorijun <123007169+HGive@users.noreply.github.com>
2024-04-09 15:56:24 +0900

    Time: 53 ms (46.54%), Space: 48.9 MB (45.62%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,9 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    var returnedArray = [] ;
-    for(var i = 0 ; i<arr.length;i++){
-        returnedArray[i] = fn(arr[i],i);
+    var returnedArray = [];
+    for(let i = 0; i<arr.length; i++){
+        returnedArray.push(fn(arr[i],i));
     }
     return returnedArray;
 };
